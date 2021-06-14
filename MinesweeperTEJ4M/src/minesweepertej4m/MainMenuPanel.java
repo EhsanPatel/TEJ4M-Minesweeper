e3f620f4259354cd9cb20d2aecd90fd519847645
Tacitor <lukasjkrampitz@gmail.com>
2021-06-14 14:02:16 -0400

    Title: Add Mouse Click Listener to Main menu Time: 1:37 Project Details: Add the framework to detect when a user click on the Main menu JPanel. Also add the logic for detecting the clicks on the three main menu buttons.

--- MinesweeperTEJ4M/src/minesweepertej4m/MainMenuPanel.java
+++ MinesweeperTEJ4M/src/minesweepertej4m/MainMenuPanel.java
@@ -7,6 +7,8 @@ package minesweepertej4m;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 import static resources.ResourcesRef.*;
 
@@ -14,47 +16,89 @@ import static resources.ResourcesRef.*;
  *
  * @author Tacitor
  */
-public class MainMenuPanel extends JPanel{
-    
+public class MainMenuPanel extends JPanel {
+
     private MainMenuFrame mainMenuFrameRef;
 
     public MainMenuPanel(MainMenuFrame m) {
         mainMenuFrameRef = m;
-        
-        
+
+        //add a mouse Listener
+        addMouseListener(new MouseAdapter() {
+            /**
+             * Trigger when the user clicks the Main menu Panel.
+             *
+             * @param evt The event representing the mouse click
+             */
+            @Override
+            public final void mouseReleased(MouseEvent evt) {
+                //send the mouse event to the click handeler
+                mouseClick(evt);
+            }
+        });
+
     }
-    
-    
-    
+
+    /**
+     * Method to handle when the user clicks on the main menu
+     *
+     * @param event
+     */
+    public void mouseClick(MouseEvent event) {
+
+        //check if any of the buttons were clicked
+        //button x and y vars
+        int btnX = super.getWidth() / 2 - MAIN_MENU_CREATE.getWidth(null) / 2;
+        int btnY;
+
+        //loop through the 3 buttons
+        for (int i = 0; i < 3; i++) {
+            
+            btnY = super.getHeight() / 2 + (MAIN_MENU_CREATE.getHeight(null) * i) + (10 * i);
+
+            //check if the click was within any of the button hitboxes
+            if (event.getX() > btnX
+                    && event.getY() > btnY
+                    && event.getX() < (btnX + MAIN_MENU_CREATE.getWidth(null))
+                    && event.getY() < (btnY + MAIN_MENU_CREATE.getHeight(null))) {
+
+                System.out.println("Click! On button: " + i);
+
+            }
+
+        }
+
+    }
+
     private void drawMenu(Graphics g) {
-        
+
         Graphics2D g2d = (Graphics2D) g;
-        
+
         //add the background image
         g2d.drawImage(MAIN_MENU_BACKGROUND, 0, 0, null);
-        
+
         //add the button images
         //add the create button
-        g2d.drawImage(MAIN_MENU_CREATE, 
-                super.getWidth() / 2 - (MAIN_MENU_CREATE.getWidth(null) / 2), 
-                super.getHeight()/ 2, null);
-        
+        g2d.drawImage(MAIN_MENU_CREATE,
+                super.getWidth() / 2 - (MAIN_MENU_CREATE.getWidth(null) / 2),
+                super.getHeight() / 2, null);
+
         //add the join button
-        g2d.drawImage(MAIN_MENU_JOIN, 
-                super.getWidth() / 2 - (MAIN_MENU_CREATE.getWidth(null) / 2), 
-                super.getHeight()/ 2 + (MAIN_MENU_CREATE.getHeight(null)) + 10, null);
-        
+        g2d.drawImage(MAIN_MENU_JOIN,
+                super.getWidth() / 2 - (MAIN_MENU_CREATE.getWidth(null) / 2),
+                super.getHeight() / 2 + (MAIN_MENU_CREATE.getHeight(null)) + 10, null);
+
         //add the settings button
-        g2d.drawImage(MAIN_MENU_JOIN, 
-                super.getWidth() / 2 - (MAIN_MENU_CREATE.getWidth(null) / 2), 
-                super.getHeight()/ 2 + (MAIN_MENU_CREATE.getHeight(null) * 2) + (10 * 2), null);
+        g2d.drawImage(MAIN_MENU_SETTINGS,
+                super.getWidth() / 2 - (MAIN_MENU_CREATE.getWidth(null) / 2),
+                super.getHeight() / 2 + (MAIN_MENU_CREATE.getHeight(null) * 2) + (10 * 2), null);
     }
-    
+
     @Override
-    public void paintComponent(Graphics g){
-        
+    public void paintComponent(Graphics g) {
+
         super.paintComponent(g); //prep the panel for drawing
         drawMenu(g); //draw the main menu
     }
-    
+
 }
