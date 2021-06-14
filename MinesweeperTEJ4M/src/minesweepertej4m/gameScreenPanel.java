fe4df3fcb3c1c36046838341e47c5da1b29ac05d
EhsanPatel <EhsanPatel@users.noreply.github.com>
2021-06-14 14:04:58 -0400

    Title: Added comments for readability Time: 7 minutes Project Details: Commented code to stay up to date and increase readability

--- MinesweeperTEJ4M/src/minesweepertej4m/gameScreenPanel.java
+++ MinesweeperTEJ4M/src/minesweepertej4m/gameScreenPanel.java
@@ -1,7 +1,7 @@
 /*
- * Ehsan Patel
+ * Ehsan Patel & Lukas Krampitz
  * 14-Jun-2021
- * and open the template in the editor.
+ * The JPanel that will be placed on the main game screen JFrame that will hold the logic for the gameplay
  */
 package minesweepertej4m;
 
@@ -12,21 +12,32 @@ import javax.swing.JPanel;
 import static resources.ResourcesRef.*;
 
 public class gameScreenPanel extends JPanel{
+    //the parent frame
     private gameScreenFrame gameScreenFrameRef;
     
+    /**
+     * constructor for this panel to be drawn
+     */
     public gameScreenPanel(gameScreenFrame m) {
+        //stores the parent frame passed in
         gameScreenFrameRef = m;
     }
     
     
     
     private void drawStaticComponents(Graphics g) {
-        
+        //The graphics model to use
         Graphics2D g2d = (Graphics2D) g;
+        //changes the drawing color to green
         g2d.setColor(new Color(88,165,100));
+        //draws the background on the panel
         g2d.fillRect(0,0,getWidth(),getHeight());
+        //changes the color to white
         g2d.setColor(new Color(255,255,255));
+        //draws the navigation bar
         g2d.fillRect(0,getHeight()-100,getWidth(),100);
+        
+        //draws all the images for buttons to be used
         g2d.drawImage(NAV_SETTINGS, 0, getHeight()-90, this);
         g2d.drawImage(NAV_SCOUT, (getWidth()/2)-(NAV_SCOUT.getWidth(this)/2)-100, getHeight()-90, this);
         g2d.drawImage(NAV_FLAG, (getWidth()/2)-(NAV_FLAG.getWidth(this)/2), getHeight()-90, this);
