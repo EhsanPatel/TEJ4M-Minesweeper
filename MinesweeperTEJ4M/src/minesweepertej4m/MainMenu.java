eb33a680e2968d275c057d2e5dfad18279672bd2
Tacitor <lukasjkrampitz@gmail.com>
2021-06-14 10:21:20 -0400

    Add basic Java class for the main menu

---
+++ MinesweeperTEJ4M/src/minesweepertej4m/MainMenu.java
@@ -0,0 +1,21 @@
+/*
+ * Ehsan Patel & Lukas Krampitz
+ * 14-Jun-2021
+ * The main menu of the MineSweeper Multiplayer Game.
+ */
+package minesweepertej4m;
+
+/**
+ *
+ * @author ehsan
+ */
+public class MainMenu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
