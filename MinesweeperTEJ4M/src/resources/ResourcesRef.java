cc48771e470e8f542f4d4e9cdebf9f5f2e1f2ca8
Tacitor <lukasjkrampitz@gmail.com>
2021-06-14 10:25:28 -0400

    add resources structure

---
+++ MinesweeperTEJ4M/src/resources/ResourcesRef.java
@@ -0,0 +1,14 @@
+/*
+ * Lukas Krampitz
+ * Jun 14, 2021
+ * A refenece class for all resources files so that they can be located even within a compiled .jar
+ */
+package resources;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class ResourcesRef {
+    
+}
